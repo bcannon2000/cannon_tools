c2faccfce8a3e12c332645d2ba21d006204a48cb
Jordan Hudgens <jordan@devcamp.com>
2017-07-11 17:01:21 +0000

    Dev toll inital commit

---
+++ lib/cannon_tools.rb
@@ -0,0 +1,6 @@
+require "cannon_tools/version"
+require "cannon_tools/renderer"
+
+module CannonTools
+  # Your code goes here...
+end
---
+++ lib/cannon_tools/renderer.rb
@@ -0,0 +1,7 @@
+module CannonTools
+  class Renderer
+    def self.copyright name, msg
+      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+    end
+  end
+end
